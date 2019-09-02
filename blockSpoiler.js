9d74a7c9aef2b4716223863bb75af18a2d9a70a5
Shristika Yadav <sy2109@g.rit.edu>
2019-09-02 01:04:45 -0700

    Add files via upload

---
+++ blockSpoiler.js
@@ -0,0 +1,32 @@
+var dictionary = ['Endgame','Tony Stark', 'Iron Man', 'Pepper Potts', 'Rescue', 'James Rhodes','War Machine','Nick Fury','Nicholas Joseph Fury','Howard Stark','Bruce Banner','Hulk','Natasha Romanoff','Black Widow','Anton Vanko','Whiplash',
+'Boris Turgenov','Crimson Dynamo','Justin Hammer','Igor Vanko','Peter Parker','Spider-Man','Thor','Jane Foster','Odin','Loki','Heimdall','Clint Barton','Hawkeye','Ronin',
+'Steve Rogers','Captain America','Johann Shmidt','Red Skull','Peggy Carter','Bucky Barnes','Winter Soldier','Hunter','White Wolf','Arnim Zola','Thanos','Wanda Maximoff','Scarlet Witch',
+'Peter Quill','Star-Lord','Gamora','Arthur Douglas','Drax the Destroyer','Rocket Raccoon','Groot','Rhomann Dey','Nova Prime','Garthan Saal','Nova Omega','Nebula','Ronan the Accuser','Korath the Pursuer',
+'Kraglin','Cosmo','Karman-Kan','Vision','Hank','Pym','Ant-Man','Scott Lang','Ant-Man','Darren Cross','Yellow jacket','Hope Pym','Red Queen','Janet van Dyne','Wasp',"T'Challa",'Black Panther','Helmut Zemo','Baron Zemo','Everett Ross',
+'Shuri','Nakia','Stephen Strange','Doctor Strange','Dormammu','Mantis','Valkyrie','Surtur','Carol Danvers','Captain Marvel','Yon-Rogg','Magnitron','Minn-Erva','Doctor Minerva','Maria Rambeau','Ikaris','Sersi','Gamora','Hawkeye'];
+
+function replace() {
+	tags = document.getElementsByTagName('*');
+	for(var i =0; i<tags.length; i++){
+		var element = tags[i];
+		for(var j=0; j<element.childNodes.length ; j++){
+			var child = element.childNodes[j];
+			if(child.nodeType === 3){
+				replaceText(element,child)
+			}
+		}
+	}
+}
+function replaceText (element, child) {
+	var text = child.nodeValue;
+	text = text.toString();
+	for(var k=0; k<dictionary.length; k++){
+		var check = text.includes(dictionary[k]);
+		if(check == true){
+			element.style.backgroundColor = 'red';
+			element.style.color = 'red';
+		}
+	}
+}
+
+replace();
